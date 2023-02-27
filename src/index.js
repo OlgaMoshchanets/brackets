8f9d03c16c30723dacbd6e97f088d0bb4696fc32
Olga Moshchanets <o.moshchanets@gmail.com>
2023-02-28 01:59:20 +0300

    feat: implement Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig1) {
+
+  const openBrackets = [];
+  const closeBrackets = [];
+  const bracketsConfig = {};
+
+  bracketsConfig1.forEach( el => {
+    openBrackets.push(el[0]);
+    closeBrackets.push(el[1]);
+
+    bracketsConfig[el[1]] = el[0];
+  })
+
+ 
+  
+  // const openBrackets = ['(', '{', '['];
+  // const closeBrackets = [')', '}', ']'];
+  // const bracketsConfig = {
+  //   [')']: '(',
+  //   ['}']: '{',
+  //   [']']: '['
+  // }
+
+  let stack = [];
+  let currentSymbol;
+ 
+
+  for (let i = 0; i < str.length; i++) {
+    currentSymbol = str[i];
+    let topElement = stack[stack.length -1];
+
+    if (closeBrackets.includes(currentSymbol)) {
+      if (stack.length === 0) {
+        if (openBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+        } else {
+          return false;
+        };
+      } else {
+        if (bracketsConfig[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          if (openBrackets.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+          } else {
+            return false;
+          };
+        }
+      }
+    } else if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    }
+  }
+  return stack.length === 0;
 }
